34ecf3a68629bd7aad5294cf0a997b3bd369179e
Ruben Dias <ruben.roque.dias@gmail.com>
2020-04-25 17:07:35 +0100

    Initial commit.

---
+++ CouchClub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CouchClub
+//
+//  Created by Ruben Dias on 25/04/2020.
+//  Copyright © 2020 Ruben Dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
