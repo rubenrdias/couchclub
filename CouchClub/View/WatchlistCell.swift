7874d9ad46d1980e237a7703ca9cdf8a40baafe0
Ruben Dias <ruben.roque.dias@gmail.com>
2020-04-25 22:46:47 +0100

    Began implementing the Watchlist cells.

--- CouchClub/View/WatchlistCell.swift
+++ CouchClub/View/WatchlistCell.swift
@@ -12,10 +12,57 @@ class WatchlistCell: UICollectionViewCell {
     
     static let reuseIdentifier = "watchlistCell"
     
+    let imageName = "avengers_1"
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-
-        contentView.backgroundColor = .red
+        
+        contentView.clipsToBounds = true
+        
+        setupImage()
+        setupTextBackground()
+    }
+    
+    override func prepareForReuse() {
+        imageView.removeFromSuperview()
+    }
+    
+    private let imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = 8
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private func setupImage() {
+        let image = UIImage(named: imageName)
+        imageView.image = image
+        
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16)
+        ])
+    }
+    
+    private let textContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private func setupTextBackground() {
+        imageView.addSubview(textContainer)
+        NSLayoutConstraint.activate([
+            textContainer.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 0.5),
+            textContainer.leftAnchor.constraint(equalTo: imageView.leftAnchor),
+            textContainer.bottomAnchor.constraint(equalTo: imageView.bottomAnchor),
+            textContainer.rightAnchor.constraint(equalTo: imageView.rightAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
