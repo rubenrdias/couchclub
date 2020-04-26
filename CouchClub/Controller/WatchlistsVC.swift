457e722c7174ee30352242a0b03bf91e7cf95f58
Ruben Dias <ruben.roque.dias@gmail.com>
2020-04-26 12:24:18 +0100

    Updated Watchlist cells spacing.

--- CouchClub/Controller/WatchlistsVC.swift
+++ CouchClub/Controller/WatchlistsVC.swift
@@ -48,12 +48,4 @@ extension WatchlistsVC: UICollectionViewDelegateFlowLayout {
         return CGSize(width: width, height: height)
     }
     
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-//        return 0
-//    }
-    
 }
