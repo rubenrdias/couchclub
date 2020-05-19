9739c83b6afd9a65a912f7675f0e027eaed2fecd
Ruben Dias <ruben.roque.dias@gmail.com>
2020-05-19 19:00:42 +0100

    Using Firebase Cloud Functions, when a chatroom is deleted, all its messages will also be deleted.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,47 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+const messagesRef = db.collection('messages');
+
+exports.chatroomDeleted = functions.firestore.document('/chatrooms/{chatroomID}')
+    .onDelete((_snapshot, context) => {
+        const chatroomID = context.params.chatroomID;
+        return new Promise((resolve, reject) => {
+            deleteChatroomMessages(chatroomID, resolve, reject);
+        });      
+    });
+
+function deleteChatroomMessages(chatroomID, resolve, reject) {
+    let query = messagesRef.where('chatroomID', '==', chatroomID).limit(100);
+
+    query.get()
+    .then((snapshot) => {
+        // When there are no documents left, we are done
+        if (snapshot.size === 0) {
+            return 0;
+        }
+
+        // Delete documents in a batch
+        let batch = db.batch();
+        snapshot.docs.forEach((doc) => {
+            batch.delete(doc.ref);
+        });
+
+        return batch.commit().then(() => {
+            return snapshot.size;
+        });
+    }).then((numDeleted) => {
+        if (numDeleted === 0) {
+            resolve();
+            return;
+        }
+
+        // Recurse on the next process tick, to avoid exploding the stack.
+        process.nextTick(() => {
+            deleteChatroomMessages(chatroomID, resolve, reject);
+        });
+    })
+    .catch(reject);
+}
